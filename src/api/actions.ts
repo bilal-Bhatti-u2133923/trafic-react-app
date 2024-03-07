db10dc4c77e09575582ba9d81f1ae8b9b534e35c
bilal-Bhatti-u2133923 <160130049+bilal-Bhatti-u2133923@users.noreply.github.com>
2024-03-07 12:21:03 +0000

    url change

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https://solid-pancake-r474rj4q4qpvh5j4p-3000.app.github.dev/";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
